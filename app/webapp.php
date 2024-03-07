7bc6a1f607a6dc41d072656dad9f4723c383b686
huroshokunin <1CJK1207@cc.u-tokai.ac.jp>
2024-03-07 19:45:58 +0900

    webapp系統の更新

---
+++ app/webapp.php
@@ -0,0 +1,15 @@
+<?php
+echo "こんにちは、世界！";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="baken_rank.html">的中率・回収率ランク付けアプリ</a>
+</body>
+</html>

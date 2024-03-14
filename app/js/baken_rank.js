745d5b942f379877307e3af0da83c5d19be74c77
huroshokunin <1CJK1207@cc.u-tokai.ac.jp>
2024-03-14 17:01:08 +0900

    微修正

--- app/js/baken_rank.js
+++ app/js/baken_rank.js
@@ -25,7 +25,7 @@ function calculateRank() {
 
     returnRateRank = rankJudge(returnRateDiff);
     hitRateRank = rankJudge(hitRateDiff);
-    resultRank = resultrankJudge(returnRateDiff + hitRateDiff);
+    resultRank = resultrankJudge(returnRateDiff + hitRateDiff) / 2;
 
     // ランクによって背景色と文字色を変更
     returnRankElement.style.backgroundColor = rankColor(returnRateRank);
